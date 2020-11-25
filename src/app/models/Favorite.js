860ad804d20c0f5b8571dd249d4ff1f981f1e48c
Aquiles Rodrigues <cyberfazer@gmail.com>
2020-11-24 22:01:18 -0300

    models

--- src/app/models/Favorite.js
+++ src/app/models/Favorite.js
@@ -15,6 +15,8 @@ class Favorite extends Model {
   static associate(models) {
     this.belongsToMany(models.User, { foreignKey: 'user_id', as: 'user' });
     this.belongsToMany(models.Production, { foreignKey: 'production_id', as: 'production' });
+
+    // A A.belongsToMany(B, { through: 'C' })associação significa que existe um relacionamento muitos-para-muitos entre Ae B, usando a tabela Ccomo tabela de junção , que terá as chaves estrangeiras ( aIde bId, por exemplo). Sequelize irá criar automaticamente este modelo C(a menos que já exista) e definir as chaves estrangeiras apropriadas nele.
   }
 }
 export default User; 
--- src/app/models/Material.js
+++ src/app/models/Material.js
@@ -13,7 +13,7 @@ class Material extends Model {
     return this;
   }
   static associate(models) {
-    this.belongsToMany(models.Produtction, { through: 'productions_materials' });
+    this.belongsToMany(models.Production, { through: 'productions_materials' });
     // Production.hasMany(Media) - associação significa que existe um relacionamento
     // Um-para-Muitos entre Production e Media, com a chave estrangeira sendo definida no modelo de destino ( Media).
     // e depois faço um apelido para type
