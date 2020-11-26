0aac5149c3da1b7132169889a6e24fe8ee31b7ba
Aquiles Rodrigues <cyberfazer@gmail.com>
2020-11-25 22:01:52 -0300

    index modificado

--- src/database/index.js
+++ src/database/index.js
@@ -1,28 +1,26 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
-/* 
-importar depois os outros models: 
 
-import { Model } from 'sequelize';
-*/
-import User from '../app/models/User';
 import TypeUser from '../app/models/TypeUser';
+import User from '../app/models/User';
 import Production from '../app/models/Production';
 import Material from '../app/models/Material';
 import Favorite from '../app/models/Favorite';
 import Media from '../app/models/Media';
 import Profile from '../app/models/Profile';
 
-const models = [TypeUser, User, Production, Material, Favorite, Media, Profile,];
+const models = [TypeUser, User, Production, Material, Favorite, Media, Profile];
+
 class Database {
   constructor() {
     this.init();
   }
+
   init() {
     this.connection = new Sequelize(databaseConfig);
 
     models
-      .map(Model => model.init(this.connection))
+      .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,9 @@ import User from './app/models/User';
 import Production from './app/models/Production';
 import Material from './app/models/Material';
 import Favorite from './app/models/Favorite';
-import Contain from './app/models/Contain';
 import Media from './app/models/Media';
 import Profile from './app/models/Profile';
+
 const Routes = new Router();
 
 Routes.get('/tipousuario', async (req, res) => {
