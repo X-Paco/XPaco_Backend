ddf931428d4388d9bcaca5591f05c35257107418
Aquiles Rodrigues <cyberfazer@gmail.com>
2020-11-27 01:25:25 -0300

    controller typeuser

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController';
 import TypeUserController from './app/controllers/TypeUserController';
 
 const routes = new Router();
+
 routes.post('/typeusers', TypeUserController.store);
 routes.post('/users', UserController.store);
 
