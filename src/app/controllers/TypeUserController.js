9dd5f2f26c6b13ed83932583822079b597b2a8d6
Aquiles Rodrigues <cyberfazer@gmail.com>
2020-11-27 01:58:28 -0300

    controller

--- src/app/controllers/TypeUserController.js
+++ src/app/controllers/TypeUserController.js
@@ -2,7 +2,16 @@ import TypeUser from '../models/TypeUser';
 
 class TypeUserController {
   async store(req, res) {
+    // TODO: validar antes de construir
+    const typeuserExiste = await TypeUser.findOne({
+      where: { description: req.body.description },
 
+    });
+    if (typeuserExiste) {
+      return res.status(400).json({ error: 'Tipo já existente.' });
+    }
+
+    // construir
     const { description } = await TypeUser.create(req.body);
 
     return res.json({
