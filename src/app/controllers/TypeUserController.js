d6e56515c713e34c8d2716ab3bb2fe6eb2cb01db
Aquiles Rodrigues <cyberfazer@gmail.com>
2020-11-27 01:37:10 -0300

    update controller typeuser

--- src/app/controllers/TypeUserController.js
+++ src/app/controllers/TypeUserController.js
@@ -2,7 +2,12 @@ import TypeUser from '../models/TypeUser';
 
 class TypeUserController {
   async store(req, res) {
-    return res.json({ ok: true });
+
+    const { description } = await TypeUser.create(req.body);
+
+    return res.json({
+      description,
+    });
   }
 }
 export default new TypeUserController(); 
